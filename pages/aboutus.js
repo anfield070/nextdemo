a2f6af35c316559e3ac56e5d4e7e8d9952c32078
Sutee Wathanathamsiri <anfield070@gmail.com>
2021-01-07 23:34:59 +0700

    123123

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -4,7 +4,7 @@ export default function Aboutus() {
   return (
     <div>
       <h1>Aboutus</h1>
-      <div className="error">Help me</div>
+      <div className="error">Help me 111</div>
 
       {/* <style jsx>{`
         .error {
